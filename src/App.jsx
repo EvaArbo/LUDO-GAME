48d89e6399b96f121d9b9b331379a15e67f2d3fe
Nijhan <najmashaaban6@gmail.com>
2025-07-23 16:45:58 +0300

    initializing project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Dice from "./components/Dice";
+import GamePiece from "./assets/Components/Gamepiece";
+
+import { GameProvider } from './context/Gamecontext';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <GameProvider>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Ludo Game</h1>
+        <Dice />
+        <GamePiece/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </GameProvider>
+  );
 }
 
-export default App
+export default App;
