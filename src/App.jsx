bd5ba80413ca90a07efe143ebe51630afa4970f3
EvaArbo <evelynearbogusti@gmail.com>
2025-07-24 00:29:21 +0300

    global polish and styled app area

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Board from "./assets/Components/Board";
-import Dice from "./components/Dice";
-import GamePiece from "./assets/Components/Gamepiece";
-import { GameProvider } from "./context/Gamecontext";
 import "./App.css";
-
 const App = () => {
   return (
+
     <GameProvider>
       <div className="app-container">
         <h1>Ludo Game</h1>
@@ -19,3 +16,5 @@ const App = () => {
 };
 
 export default App;
+
+    
